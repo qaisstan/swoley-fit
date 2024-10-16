60cbd482c1fdd40377f5485b953f8b1744fc0edc
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-16 18:41:19 +0200

    utils tempos

---
+++ src/components/Generator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionWrapper from './SectionWrapper'
+
+export default function Generator() {
+  return (
+    
+    <SectionWrapper header={"generate your workout"}
+    title = {['It\'s', 'Huge', 'o\'clock']}>
+    
+    </SectionWrapper>
+
+  )
+}
---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SectionWrapper(props) {
+
+      const {children, header, title} = props
+
+  return (
+    <section className='min-h-screen flex flex-col gap-10'>
+      <div></div>
+
+    </section>
+  )
+}
